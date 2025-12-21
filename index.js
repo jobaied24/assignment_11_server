d9304e99f318e2d727c54e053fa33e08e2408900
hero00 <jobaied543@gmail.com>
2025-12-22 00:45:04 +0600

    completed all server side task

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function run() {
     // })
 
 
-    app.get('/items/:id',verifyToken,async(req,res)=>{
+    app.get('/items/:id',async(req,res)=>{
       const id = req.params.id;
  
       const query = {_id : new ObjectId(id)};
